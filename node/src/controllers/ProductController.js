065c23f3fe50ed27d9a42f6e7352347cf7c01855
Ricardo Salles <ricardo.salles@gmail.com>
2020-06-29 00:38:22 -0300

    Tests with nodeJS

---
+++ node/src/controllers/ProductController.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Product = mongoose.model("Product");
+
+module.exports = {
+  async index(request, response) {
+    const { page = 1, limit = 10 } = request.query;
+    const product = await Product.paginate(
+      {},
+      { page, limit: parseInt(limit) }
+    );
+
+    return response.json(product);
+  },
+  async show(request, response) {
+    const product = await Product.findById(request.params.id);
+    return response.json(product);
+  },
+  async store(request, response) {
+    const product = await Product.create(request.body);
+    return response.json(product);
+  },
+  async update(request, response) {
+    const product = await Product.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      { new: true }
+    );
+    return response.json(product);
+  },
+  async destroy(request, response) {
+    const product = await Product.findByIdAndRemove(request.params.id);
+    return response.send();
+  },
+};
---
+++ node/src/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const routes = express.Router();
+
+const ProductControllers = require("./controllers/ProductController");
+
+routes.get("/products", ProductControllers.index);
+routes.get("/products/:id", ProductControllers.show);
+routes.post("/products", ProductControllers.store);
+routes.put("/products/:id", ProductControllers.update);
+routes.delete("/products/:id", ProductControllers.destroy);
+
+module.exports = routes;
